2b52c7778450184eda6cb5f71e23cf439135c7c2
echo <coderecho1@gmail.com>
2022-06-14 13:04:45 +0800

    fix: 解决冲突

--- src/orbiterCore.js
+++ src/orbiterCore.js
@@ -86,7 +86,43 @@ function isLimitNumber(chain) {
   return false
 }
 
+
+/**
+ * @param {number} precision
+ */
+ function getDigitByPrecision(precision) {
+  return precision === 18 ? 6 : 2
+}
+
+const precisionResolverMap = {
+  // pay attention:  the type of field "userAmount" in the following methods is not BigNumber 
+  // but string in decimal!!!
+  "18": userAmount => userAmount.slice(0, 6),
+  "default": userAmount => userAmount
+}
+
+/**
+ * @description {
+ *  This method is to confirm the legitimacy of the amount
+ *  if the amount u passed is legal, it will return it intact
+ *  otherwise the data we processed will be returned
+ * }
+ * @param userAmount the amount user given
+ * @param chain config of the current chain 
+ */
+const performUserAmountLegality = (userAmount, chain) => {
+  const { precision } = chain;
+  const decimalData = userAmount.toFormat(); // convert BigNumber instance to decimal
+  // if the precision that current chain support equals 18, the maximum precision of userAmount u passed is 6
+  const matchResolver = precisionResolverMap[precision] || precisionResolverMap["default"];
+  // eg: precision equals 18, but the value of userAmount is 0.3333333333
+  // covert result after matchResolver processed was 0.333333
+  const convertResult =  matchResolver(decimalData, chain); 
+  return new BigNumber(convertResult);
+}
+
 function getToAmountFromUserAmount(userAmount, selectMakerInfo, isWei) {
+  userAmount = performUserAmountLegality(userAmount, selectMakerInfo);
   let toAmount_tradingFee = new BigNumber(userAmount).minus(
     new BigNumber(selectMakerInfo.tradingFee)
   )
@@ -412,13 +448,6 @@ function isAmountValid(chain, amount) {
   }
 }
 
-/**
- * @param {number} precision
- */
-function getDigitByPrecision(precision) {
-  return precision === 18 ? 6 : 2
-}
-
 module.exports = {
   getPTextFromTAmount,
   getToChainIDFromAmount,
@@ -432,3 +461,4 @@ module.exports = {
   getToAmountFromUserAmount,
   getDigitByPrecision,
 }
+
